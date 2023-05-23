c6dfb78c73cf982d91de689927b2346f3256db9e
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-05-23 20:46:26 +0700

    Initial Commit

---
+++ iExercise12-CustomView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iExercise12-CustomView
+//
+//  Created by Minh Tan Vu on 23/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
